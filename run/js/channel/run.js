58325e21dbb640e22b6f1f101cd6108ab31901ee
qianghuang <hfhqhb@163.com>
2014-09-18 08:51:47 +0800

    create new project:run

---
+++ run/js/channel/run.js
@@ -0,0 +1,11 @@
+function initPage() {
+	var pageWidth = $(window).width();
+	var pageHeight = $(window).height();
+	$(".wrapper").css({
+		height:pageHeight
+	});
+	$(".sec").css({
+		height:pageHeight
+	});
+}
+initPage();
