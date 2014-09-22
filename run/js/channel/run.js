89e71de47cd79e634a337c338dc19eb09f32f93e
qianghuang <hfhqhb@163.com>
2014-09-22 11:39:21 +0800

    修改了run项目的信息

--- run/js/channel/run.js
+++ run/js/channel/run.js
@@ -147,3 +147,53 @@ function showText($ele) {
 		i++;
 	},(i+1)*500);
 }
+
+function onBridgeReady() {
+	var t = new Date().getTime(),
+		mainTitle="奔跑吧兄弟",
+	    mainDesc="《奔跑吧兄弟》是浙江卫视全新推出的大型户外竞技真人秀节目",
+	    mainURL="http://dev.hotkeypower.com/lfhtml/running/views/run.html?v=" + t,
+	    mainImgUrl= "http://dev.hotkeypower.com/lfhtml/running/images/run_share.png";
+	
+	//转发朋友圈
+	WeixinJSBridge.on("menu:share:timeline", function(e) {
+	    var data = {
+	        img_url:mainImgUrl,
+	        img_width: "120",
+	        img_height: "120",
+	        link: mainURL,
+	        //desc这个属性要加上，虽然不会显示，但是不加暂时会导致无法转发至朋友圈，
+	        desc: mainDesc,
+	        title: mainTitle
+	    };
+	    WeixinJSBridge.invoke("shareTimeline", data, function(res) {
+	        WeixinJSBridge.log(res.err_msg)
+	    });
+	});
+	//同步到微博
+	WeixinJSBridge.on("menu:share:weibo", function() {
+	    WeixinJSBridge.invoke("shareWeibo", {
+	        "content": mainDesc,
+	        "url": mainURL
+	    }, function(res) {
+	        WeixinJSBridge.log(res.err_msg);
+	    });
+	});
+	//分享给朋友
+	WeixinJSBridge.on('menu:share:appmessage', function(argv) {
+	    WeixinJSBridge.invoke("sendAppMessage", {
+	        img_url: mainImgUrl,
+	        img_width: "120",
+	        img_height: "120",
+	            link: mainURL,
+	            desc: mainDesc,
+	            title: mainTitle
+	        }, function(res) {
+	            WeixinJSBridge.log(res.err_msg)
+	        });
+	    });
+};
+//执行
+document.addEventListener('WeixinJSBridgeReady', function() {
+    onBridgeReady();
+});
